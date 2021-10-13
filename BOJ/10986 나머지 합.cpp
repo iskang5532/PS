7c21de0b88b9b527f44eb518d579f725fe9c639d
InSeok <iskang5532@naver.com>
2021-10-13 23:56:36 +0900

    PS

--- BOJ/10986 나머지 합.cpp
+++ BOJ/10986 나머지 합.cpp
@@ -1,9 +1,10 @@
 /* 
  * 10986 나머지 합
- * 누적합
+ * 누적합, 수학
  * 시간 복잡도: ?
  * 문제: https://www.acmicpc.net/problem/10986
  * ETC.) 무식하게 기존 누적합 방식으로 풀어 봤지만, 시간 초과가 남
+ * 10.13) 나중에 다시 풀어보기로
  * 참고: https://blog.naver.com/jhc9639/221411875667 (설명 및 코드)
  * https://www.acmicpc.net/board/view/63744 (long long 문법에 대한 고찰)
  */
@@ -21,15 +22,15 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    int n, m; // 1 <= n <= 1M, 2 <= m <= 1k
+    int n, m; // 수의 개수 (1 ≤ n ≤ 1M), 나눌 값 (2 ≤ m ≤ 1k)
     cin >> n >> m;
 
     vector<int> pSum(n + 1);
     pSum[0] = 0;
     for (int i = 1; i <= n; i++)
     {
-        int k;
-        cin >> k; // 0 <= 1G
+        int k; // 0 ≤ k ≤ 1G
+        cin >> k;
 
         pSum[i] = (pSum[i - 1] + k) % m; // 보기 편하게 사전에 m의 나머지 값을 저장
     }
@@ -41,7 +42,7 @@ int main()
     for (const auto &elem : pSum)
         cnt[elem]++;
 
-    ll answer = 0; // 최대 (n(n - 1))/2 (계산 과정에서 int 범위를 벗어남)
+    ll answer = 0; // 최대 n(n-1) ÷ 2 (계산 과정에서 int 범위를 벗어남)
     for (const auto i : cnt)
         answer += (1LL * i * (i - 1)) >> 1; // i개 중 2개(좌측, 우측)를 뽑는 경우의 수
 
